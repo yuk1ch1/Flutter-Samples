221ed5eebac280a2579e81600e796538f186a52a
yuk1ch1 <sma2ww@gmail.com>
2022-06-02 22:24:59 +0900

    impl: 固定のtodoリストを表示するところまで完了
    コミットもれ

--- riverpod_todo/lib/main.dart
+++ riverpod_todo/lib/main.dart
@@ -3,67 +3,62 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_todo/todo.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ProviderScope(child: const App()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+//     );
+//   }
+// }
+
+final todoListProvider =
+    StateNotifierProvider<ToDoListNotifier, List<Todo>>((ref) {
+  return ToDoListNotifier([
+    Todo(id: "1", name: "テスト", isCompleted: false),
+    Todo(id: "2", name: "テスト2", isCompleted: false),
+    Todo(id: "3", name: "テスト3", isCompleted: true),
+  ]);
+});
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return MaterialApp(title: "NavigatorApp", home: ToDoListView());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+class ToDoListView extends ConsumerWidget {
+  const ToDoListView({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    // rebuild the widget when the todo list changes
+    List<Todo> todos = ref.watch(todoListProvider);
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: new AppBar(
+          title: new Text('ToDoList'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: ListView(children: [
+      for (final todo in todos)
+        CheckboxListTile(
+          value: todo.isCompleted,
+          // when tapping on the todo, change its completed status
+          onChanged: (value) =>
+              ref.read(todoListProvider.notifier).toggle(todo.id),
+          title: Text(todo.name),
+        )
+    ]));
   }
 }
