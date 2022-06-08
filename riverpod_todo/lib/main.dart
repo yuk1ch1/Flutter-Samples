4d1729df19205cda55e1b3265f3c57e85ed3bc02
yuk1ch1 <sma2ww@gmail.com>
2022-06-08 19:38:22 +0900

    impl: チェックマークの入ってないtodoの数を表示するように実装

--- riverpod_todo/lib/main.dart
+++ riverpod_todo/lib/main.dart
@@ -12,6 +12,27 @@ final todoListProvider =
     Todo(id: "1", name: "テスト", isCompleted: false),
     Todo(id: "2", name: "テスト2", isCompleted: false),
     Todo(id: "3", name: "テスト3", isCompleted: true),
+    Todo(id: "4", name: "テスト", isCompleted: false),
+    Todo(id: "5", name: "テスト2", isCompleted: false),
+    Todo(id: "6", name: "テスト3", isCompleted: true),
+    Todo(id: "7", name: "テスト", isCompleted: false),
+    Todo(id: "8", name: "テスト2", isCompleted: false),
+    Todo(id: "9", name: "テスト3", isCompleted: true),
+    Todo(id: "10", name: "テスト", isCompleted: false),
+    Todo(id: "11", name: "テスト2", isCompleted: false),
+    Todo(id: "12", name: "テスト3", isCompleted: true),
+    Todo(id: "14", name: "テスト", isCompleted: false),
+    Todo(id: "15", name: "テスト2", isCompleted: false),
+    Todo(id: "16", name: "テスト3", isCompleted: true),
+    Todo(id: "17", name: "テスト", isCompleted: false),
+    Todo(id: "18", name: "テスト2", isCompleted: false),
+    Todo(id: "19", name: "テスト3", isCompleted: true),
+    Todo(id: "20", name: "テスト", isCompleted: false),
+    Todo(id: "21", name: "テスト2", isCompleted: false),
+    Todo(id: "22", name: "テスト3", isCompleted: true),
+    Todo(id: "23", name: "テスト", isCompleted: false),
+    Todo(id: "24", name: "テスト2", isCompleted: false),
+    Todo(id: "25", name: "テスト3", isCompleted: true),
   ]);
 });
 
@@ -31,6 +52,9 @@ class ToDoListView extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     // rebuild the widget when the todo list changes
     List<Todo> todos = ref.watch(todoListProvider);
+    int leftTodosCount =
+        todos.where((todo) => !todo.isCompleted).toList().length;
+
     return Scaffold(
         body: Padding(
       padding: const EdgeInsets.all(24.0),
@@ -50,6 +74,12 @@ class ToDoListView extends ConsumerWidget {
               decoration: const InputDecoration(
             hintText: 'what needs to be done?',
           )),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 30),
+          ),
+          Row(
+            children: [Text('$leftTodosCount' + ' items left')],
+          ),
           for (final todo in todos)
             CheckboxListTile(
               value: todo.isCompleted,
