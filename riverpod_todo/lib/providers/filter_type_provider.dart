a157d5c61275a447c500de51d51680814bbfaa78
yuk1ch1 <sma2ww@gmail.com>
2022-06-18 17:01:14 +0900

    add: filter

---
+++ riverpod_todo/lib/providers/filter_type_provider.dart
@@ -0,0 +1,11 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+enum Filter {
+  notCompleted,
+  completed,
+  all
+}
+
+final filterProvider = StateProvider<Filter>(
+    (ref) => Filter.notCompleted
+);
